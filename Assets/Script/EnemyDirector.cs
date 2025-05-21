9fda3e2083551d6231030660214cc48bf09a06a6
阿久津匠輝 <shoki.akutsu.vantan@gmail.com>
2025-05-21 13:21:47 +0900

    [add] コミット, EnemyDirector追加

--- Assets/Script/EnemyDirector.cs
+++ Assets/Script/EnemyDirector.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class EnemyDirector : MonoBehaviour
 {
     [SerializeField] List<GameObject> enemyList;
-    [SerializeField] int maxEnemy = 10;
+    //[SerializeField] int maxEnemy = 10;
+
+    int frame = 0;
+    [SerializeField] int generateFrame = 30;
+    [SerializeField] float radius = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,20 @@ public class EnemyDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        frame++;
+
+        if (frame > generateFrame) 
+        {
+            frame = 0;
+
+			// ランダムな角度を生成
+			float randomAngle = Random.Range(0f, 360f);
+
+            // 極座標からデカルト座標に変換
+            float x = Mathf.Cos(randomAngle * Mathf.Deg2Rad) * radius;
+            float y = Mathf.Sin(randomAngle * Mathf.Deg2Rad) * radius;
+
+            Instantiate(enemyList[0], new Vector2 (x, y), Quaternion.identity);
+		}
+	}
 }
